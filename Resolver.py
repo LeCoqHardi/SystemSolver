12e682eaff2a41e77e9197e43497b72deabd1673
LeCoqHardi <theo21.man@gmail.com>
2021-09-30 10:44:38 +0200

    update2

---
+++ Resolver.py
@@ -0,0 +1,5 @@
+from sympy import *
+
+# Resolver est un script python permettant de résoudre des équations 
+# Créé par LeCoqHardi, disponible sur Github.
+# https://www.twitch.tv/lecoqhardi // https://www.twitter.com/LeCoqHardi__ 
